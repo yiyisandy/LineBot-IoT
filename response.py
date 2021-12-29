fbde1dc3f5c32e3cef6b6b734b0a3b9159135d3b
yiyisandy <94918661+yiyisandy@users.noreply.github.com>
2021-12-29 14:35:23 +0800

    Update response.py

--- response.py
+++ response.py
@@ -21,7 +21,7 @@ def make_response(user: User, message: str, attachment_path: str, attachment_ext
     
     
     if message == 'read':
-         gc = pygsheets.authorize(service_file='googlesheet.json')
+        gc = pygsheets.authorize(service_file='googlesheet.json')
 
         sht = gc.open_by_url(
             'https://docs.google.com/spreadsheets/d/1DflljkJf-o_dZy9Td-3c03_bAfocSfRWbgjT3t9C1ig/'
